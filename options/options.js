ddcb82aa77e5a86cc9c9b2c7d5a844a5c94a0c1a
Crowdbotics <team@crowdbotics.com>
2023-10-23 15:18:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "youth-lead-georgia-44110",
+  url: "https://youth-lead-georgia-44110.botics.co",
+  api: "https://youth-lead-georgia-44110.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
